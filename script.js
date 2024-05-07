df5b7515e7fbccb5dbce2aab4d401cd034d79192
Shashi Kumar <shashikumarmutapelli@gmail.com>
2024-05-07 22:38:49 +0530

    solved the error in js file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let boxes=document.querySelectorAll(".box");
-let winningArray =i [[0,1,2],[0,4,8],[0,3,6],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[6,7,8]];
+let winningArray =[[0,1,2],[0,4,8],[0,3,6],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[6,7,8]];
 let oturn=true;
 let body=document.body;
 let j= document.createElement("h1");
